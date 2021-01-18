4ac87c39f6383866ecc29b459cb1700594f0a94e
Dora <domonshieh@gmail.com>
2021-01-18 17:45:28 +0800

    Initial Commit

---
+++ 10YearChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10YearChallenge
+//
+//  Created by 謝孟芩 on 2021/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
